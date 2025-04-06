117e7b3499758665a9ec06f172b6f8a04fbe7553
mmrdr <isaev2k18@gmail.com>
2025-04-06 20:39:29 +0300

    Created cell for messages

---
+++ chakchat/Screens/Chat/ChatModels.swift
@@ -0,0 +1,8 @@
+//
+//  ChatModels.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 06.04.2025.
+//
+
+import Foundation
---
+++ chakchat/UI/UIComponents/UIChatMessageCell.swift
@@ -0,0 +1,8 @@
+//
+//  UIChatMessageCell.swift
+//  chakchat
+//
+//  Created by Кирилл Исаев on 06.04.2025.
+//
+
+import Foundation
