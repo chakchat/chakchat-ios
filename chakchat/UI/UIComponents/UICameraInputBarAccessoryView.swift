60d22040d323ca3250e6ad0c8b7cfe637a97d5e9
mmrdr <isaev2k18@gmail.com>
2025-04-09 14:01:00 +0300

    UI chore

--- chakchat/UI/UIComponents/UICameraInputBarAccessoryView.swift
+++ chakchat/UI/UIComponents/UICameraInputBarAccessoryView.swift
@@ -5,4 +5,112 @@
 //  Created by Кирилл Исаев on 08.04.2025.
 //
 
-import Foundation
+import InputBarAccessoryView
+import UIKit
+
+protocol CameraInputBarAccessoryViewDelegate: InputBarAccessoryViewDelegate {
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith attachments: [AttachmentManager.Attachment])
+}
+
+extension CameraInputBarAccessoryViewDelegate {
+    func inputBar(_: InputBarAccessoryView, didPressSendButtonWith _: [AttachmentManager.Attachment]) { }
+}
+
+// MARK: - CameraInputBarAccessoryView
+
+class CameraInputBarAccessoryView: InputBarAccessoryView {
+    // MARK: Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Internal
+    
+    lazy var attachmentManager: AttachmentManager = { [unowned self] in
+        let manager = AttachmentManager()
+        manager.delegate = self
+        return manager
+    }()
+    
+    func configure() {
+        let camera = makeButton(named: "ic_camera")
+        camera.tintColor = .darkGray
+        setLeftStackViewWidthConstant(to: 35, animated: true)
+        setStackViewItems([camera], forStack: .left, animated: false)
+        inputPlugins = [attachmentManager]
+    }
+    
+    override func didSelectSendButton() {
+        if attachmentManager.attachments.count > 0 {
+            (delegate as? CameraInputBarAccessoryViewDelegate)?
+                .inputBar(self, didPressSendButtonWith: attachmentManager.attachments)
+        }
+        else {
+            delegate?.inputBar(self, didPressSendButtonWith: inputTextView.text)
+        }
+    }
+    
+    // MARK: Private
+    
+    private func makeButton(named _: String) -> InputBarButtonItem {
+        InputBarButtonItem()
+            .configure {
+                $0.spacing = .fixed(10)
+                $0.image = UIImage(systemName: "camera.fill")?.withRenderingMode(.alwaysTemplate)
+                $0.setSize(CGSize(width: 30, height: 30), animated: false)
+            }.onSelected {
+                $0.tintColor = .systemBlue
+            }.onDeselected {
+                $0.tintColor = UIColor.lightGray
+            }.onTouchUpInside { _ in
+                print("Item Tapped")
+            }
+    }
+}
+
+extension CameraInputBarAccessoryView: AttachmentManagerDelegate {
+    // MARK: - AttachmentManagerDelegate
+    
+    func attachmentManager(_: AttachmentManager, shouldBecomeVisible: Bool) {
+        setAttachmentManager(active: shouldBecomeVisible)
+    }
+    
+    func attachmentManager(_ manager: AttachmentManager, didReloadTo _: [AttachmentManager.Attachment]) {
+        sendButton.isEnabled = manager.attachments.count > 0
+    }
+    
+    func attachmentManager(_ manager: AttachmentManager, didInsert _: AttachmentManager.Attachment, at _: Int) {
+        sendButton.isEnabled = manager.attachments.count > 0
+    }
+    
+    func attachmentManager(_ manager: AttachmentManager, didRemove _: AttachmentManager.Attachment, at _: Int) {
+        sendButton.isEnabled = manager.attachments.count > 0
+    }
+    
+    func setAttachmentManager(active: Bool) {
+        let topStackView = topStackView
+        if active, !topStackView.arrangedSubviews.contains(attachmentManager.attachmentView) {
+            topStackView.insertArrangedSubview(attachmentManager.attachmentView, at: topStackView.arrangedSubviews.count)
+            topStackView.layoutIfNeeded()
+        } else if !active, topStackView.arrangedSubviews.contains(attachmentManager.attachmentView) {
+            topStackView.removeArrangedSubview(attachmentManager.attachmentView)
+            topStackView.layoutIfNeeded()
+        }
+    }
+}
+
+// MARK: UIAdaptivePresentationControllerDelegate
+
+extension CameraInputBarAccessoryView: UIAdaptivePresentationControllerDelegate {
+    // Swipe to dismiss image modal
+    public func presentationControllerWillDismiss(_: UIPresentationController) {
+        isHidden = false
+    }
+    
+}
