0b584c327759b4b8b254f186d94d8ffe041f7367
mmrdr <isaev2k18@gmail.com>
2025-04-18 11:10:21 +0300

    And this is also file cell

--- chakchat/UI/UIComponents/UIFileMessageCell.swift
+++ chakchat/UI/UIComponents/UIFileMessageCell.swift
@@ -5,4 +5,111 @@
 //  Created by Кирилл Исаев on 18.04.2025.
 //
 
-import Foundation
+import UIKit
+import MessageKit
+
+class FileMessageCell: TextMessageCell {
+    
+    weak var cellDelegate: FileMessageEditMenuDelegate?
+    
+    private var messageStatus: UILabel = UILabel()
+    private var fileImageView: UIImageView = UIImageView()
+    private var fileURL: URL?
+    
+    override func setupSubviews() {
+        super.setupSubviews()
+        configureCell()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override func configure(with message: any MessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
+        super.configure(with: message, at: indexPath, and: messagesCollectionView)
+        
+        addGesture()
+        
+        if case .custom(let data) = message.kind {
+            guard let data = data as? FileItem else { return }
+            self.fileURL = data.url
+            messageLabel.text = fileURL?.lastPathComponent
+            
+            let fileExtension = fileURL?.pathExtension.lowercased()
+            switch fileExtension {
+            case "pdf":
+                fileImageView.image = UIImage(systemName: "doc.richtext.fill")
+            case "jpg", "jpeg", "png", "gif":
+                fileImageView.image = UIImage(systemName: "photo.fill")
+            case "txt", "rtf":
+                fileImageView.image = UIImage(systemName: "doc.text.fill")
+            default:
+                fileImageView.image = UIImage(systemName: "document.circle.fill")
+            }
+            
+            messageLabel.pinLeft(fileImageView.trailingAnchor, 5)
+        }
+    }
+    
+    private func configureCell() {
+        configureFileImageView()
+    }
+    
+    private func configureFileImageView() {
+        messageContainerView.addSubview(fileImageView)
+        fileImageView.contentMode = .scaleAspectFit
+        fileImageView.image = UIImage(systemName: "document.circle.fill")
+        fileImageView.tintColor = .blue
+        fileImageView.isUserInteractionEnabled = true
+        fileImageView.setWidth(30)
+        fileImageView.setHeight(30)
+        fileImageView.pinLeft(messageContainerView.leadingAnchor, 5)
+        fileImageView.pinCenterY(messageContainerView)
+    }
+    
+    private func addGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleFileIconTap))
+        fileImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    private func open(_ url: URL) {
+        let documentController = UIDocumentInteractionController(url: url)
+        documentController.delegate = self
+        documentController.presentPreview(animated: true)
+    }
+    
+    @objc private func handleFileIconTap() {
+        guard let fileURL = fileURL else { return }
+        open(fileURL)
+    }
+}
+
+extension FileMessageCell: UIDocumentInteractionControllerDelegate {
+    
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        guard let root = getRootViewController() else {
+            print("Unlucky")
+            return UIViewController()
+        }
+        return root
+    }
+    
+    func getRootViewController() -> UIViewController? {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let rootViewController = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
+            print("Unlucky")
+            return nil
+        }
+        return rootViewController
+    }
+}
+
+class FileMessageCellSizeCalculator: TextMessageSizeCalculator {
+    
+    override func messageContainerSize(for message: any MessageType, at indexPath: IndexPath) -> CGSize {
+        var size = super.messageContainerSize(for: message, at: indexPath)
+        size.height += 20
+        return size
+    }
+    
+}
