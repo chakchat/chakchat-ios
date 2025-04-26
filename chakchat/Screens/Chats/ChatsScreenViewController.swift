d3321d8d1a884d626ed7947f94f8a094cb0ec9f6
mmrdr <isaev2k18@gmail.com>
2025-04-26 07:26:33 +0300

    UPS

--- chakchat/Screens/Chats/ChatsScreenViewController.swift
+++ chakchat/Screens/Chats/ChatsScreenViewController.swift
@@ -91,21 +91,13 @@ final class ChatsScreenViewController: UIViewController {
     func addNewChat(_ chatData: ChatsModels.GeneralChatModel.ChatData) {
         chatsData?.insert(chatData, at: 0)
         chatsTableView.reloadData()
-        
-        chatsTableView.performBatchUpdates({
-            chatsTableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
-        }, completion: { [weak self] _ in
-            self?.chatsTableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
-        })
     }
     
     func deleteChat(_ chatID: UUID) {
-        guard let index = chatsData?.firstIndex(where: { $0.chatID == chatID }) else { return }
-        
-        chatsTableView.performBatchUpdates({
-            chatsData?.remove(at: index)
-            chatsTableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-        }, completion: nil)
+        if let i = chatsData?.firstIndex(where: {$0.chatID == chatID}) {
+            chatsData?.remove(at: i)
+            chatsTableView.reloadData()
+        }
     }
     
     func changeChatPreview(_ event: WSUpdateEvent) {
